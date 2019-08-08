5d476b02e9eddc277a07385299d33a0efe98e648
Krish <codesorter2015@gmail.com>
2019-08-08 17:58:47 +0530

    Update http.js

--- http.js
+++ http.js
@@ -2,4 +2,16 @@ HTTP Basic authentication is the simplest method for enforcing access controls t
 Rather it uses standard fields in the HTTP header for handshakes.
 The browser generates a popup window when a server requires username and passeword to authenticate 
 
+If you navigate to the endpoint https://httpbin.org/basic-auth/user/passwd in your browser, it should prompt you to enter a username and password
+the default username is "user" and the default password is "passwd". 
+
+the server should respond with the following json
+{ 
+  "authenticated" : true,
+  "user" : "user"
+}
+
+if you don't provide any credentials or if you provide wrong credentials, the server rejects the access to protected resource and responds with an HTTP Status code of 401
+
+
 
