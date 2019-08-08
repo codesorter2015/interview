8405e88008fb017a28b5e1effac68c63154d4327
Krish <codesorter2015@gmail.com>
2019-08-08 17:27:10 +0530

    Create http.js

---
+++ http.js
@@ -0,0 +1,5 @@
+HTTP Basic authentication is the simplest method for enforcing access controls to a web resource because it does not require cookies,session identifiers or login pages.
+Rather it uses standard fields in the HTTP header for handshakes.
+The browser generates a popup window when a server requires username and passeword to authenticate 
+
+
